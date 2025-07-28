5ca293fe1a9ea06ef45f966cda0b38ef2328cf0c
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2025-07-28 14:55:11 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,8 +34,8 @@ export default defineConfig({
 				},
 				{
 					userAgent: ["*"],
-					allow: ["/"],
-					disallow: ["/?*"],
+					// allow: ["/"],
+					disallow: ["*?*"],
 				},
 			],
 		}),
